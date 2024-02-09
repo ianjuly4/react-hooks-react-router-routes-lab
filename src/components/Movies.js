9b0e1cdf02266461d46f7b6a6a261b80381e4a73
Ian July <ianjuly4@gmail.com>
2024-02-09 11:54:36 -0700

    v5 router routes lab

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  console.log(movies)
+  return (
+    <div>
+    <h1>Movies Page</h1>
+    {movies.map((movie)=>{
+      return <div key={movie.title} >
+        <h2> Name: {movie.title}</h2>
+       <h4>Time: {movie.time}</h4>
+       <ul>Genres: {movie.genres.map((genre)=>{
+        return <li key={genre}>{genre}</li>
+       })}
+       </ul>
+      </div>
+    })}
+    </div>
+  )
 }
-
 export default Movies;
